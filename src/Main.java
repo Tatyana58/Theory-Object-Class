a893b8dd422a5008cd36fa8302f3b49efe601d69
Tatyana58 <tatbir@rambler.ru>
2022-12-12 22:13:13 +0300

    Beginning Начало Создали класс Person Создали метод createPerson Удалили методы name и age Перенесли метод в класс Person Переименовали метод createPerson в Person Корректировка класса Person использование this Создание новой перем John Геттеры и private Сеттеры Оптимизация программного кода- Убрали лишнее Вывели данные по Саре и Джона

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Практика Создание Класса и Объекта");
-        //вариант 1
-        String nameJohn = "John";
-        int ageJohn=13;
-        String nameSarah = "Sarah";
-        int ageSarah=30;
-        //вариант 2 - создаем 2 массива с Именем и возрастом
-        String[] names = {"John","Sarah"};
-        int[] ages = {13,30};
-        for (int i=0; i< names.length;i++){
-            System.out.println(" Имя - " + names[i] + " Возраст -" + ages[i]);
-        }
         Person sarah = new Person(" Sarah",30);
         System.out.println("sarah.name = " + sarah.getName());
         System.out.println("sarah.age = " + sarah.getAge());
         sarah.setAge(31);
         System.out.println("sarah.getAge() = " + sarah.getAge());
         Person john=new Person( "John",13);
+        System.out.println("Имя: " + john.getName()+" Возраст: " + john.getAge());
+
     }
 }
