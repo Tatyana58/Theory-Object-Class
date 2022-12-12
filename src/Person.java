8442b0049cf2e23162419889752c80464a4f7fd9
Tatyana58 <tatbir@rambler.ru>
2022-12-12 21:53:57 +0300

    Beginning Начало Создали класс Person Создали метод createPerson Удалили методы name и age Перенесли метод в класс Person Переименовали метод createPerson в Person Корректировка класса Person использование this

--- src/Person.java
+++ src/Person.java
@@ -1,10 +1,8 @@
 public class Person {
     String name;
     int age;
-    public static Person Person(String name,int age) {
-        Person person = new Person();
-        person.name = name;
-        person.age = age;
-        return person;
+    public Person(String name,int age) {
+        this.name = name;
+        this.age = age;
     }
 }
